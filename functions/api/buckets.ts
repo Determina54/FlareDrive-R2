c1d2ee94bfabbad1a83de884ad91c0f5ca07109b
Di Wusi <Determination54@outlook.com>
2026-02-28 09:48:58 +0000

    自定义s3

--- functions/api/buckets.ts
+++ functions/api/buckets.ts
@@ -8,9 +8,23 @@ async function getCurrentBucket(context) {
   if (!(await env[driveid].head("_$flaredrive$/CNAME")))
     await env[driveid].put("_$flaredrive$/CNAME", url.hostname);
 
-  const client = new S3Client(env.AWS_ACCESS_KEY_ID, env.AWS_SECRET_ACCESS_KEY);
+  // 判断是否使用第三方 S3
+  const isCustomS3 = env.CustomS3 === "true";
+  const accessKey = isCustomS3 ? env.S3_ACCESS_KEY : env.AWS_ACCESS_KEY_ID;
+  const secretKey = isCustomS3 ? env.S3_SECRET_KEY : env.AWS_SECRET_ACCESS_KEY;
+  const s3Endpoint = isCustomS3 ? env.S3_ENDPOINT : `https://${env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+
+  const client = new S3Client(accessKey, secretKey);
+
+  // 第三方 S3 如果指定了 bucket 名称，直接使用
+  if (isCustomS3 && env.S3_BUCKET_NAME) {
+    return new Response(env.S3_BUCKET_NAME, {
+      headers: { "cache-control": "max-age=604800" },
+    });
+  }
+
   const bucketsResponse = await client.s3_fetch(
-    `https://${env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
+    `${s3Endpoint}/`
   );
   const bucketsText = await bucketsResponse.text();
   const bucketNames = [
@@ -22,7 +36,7 @@ async function getCurrentBucket(context) {
         new Promise<string>((resolve, reject) => {
           client
             .s3_fetch(
-              `https://${env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com/${name}/_$flaredrive$/CNAME`
+              `${s3Endpoint}/${name}/_$flaredrive$/CNAME`
             )
             .then((response) => response.text())
             .then((text) => {
@@ -46,12 +60,26 @@ export async function onRequestGet(context) {
     const url = new URL(request.url);
     if (url.searchParams.has("current")) return await getCurrentBucket(context);
 
+    // 判断是否使用第三方 S3
+    const isCustomS3 = env.CustomS3 === "true";
+    const accessKey = isCustomS3 ? env.S3_ACCESS_KEY : env.AWS_ACCESS_KEY_ID;
+    const secretKey = isCustomS3 ? env.S3_SECRET_KEY : env.AWS_SECRET_ACCESS_KEY;
+    const s3Endpoint = isCustomS3 ? env.S3_ENDPOINT : `https://${env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+
     const client = new S3Client(
-      env.AWS_ACCESS_KEY_ID,
-      env.AWS_SECRET_ACCESS_KEY
+      accessKey,
+      secretKey
     );
+    
+    // 第三方 S3 如果指定了 bucket 名称，直接使用该 bucket 路径
+    if (isCustomS3 && env.S3_BUCKET_NAME) {
+      return client.s3_fetch(
+        `${s3Endpoint}/${env.S3_BUCKET_NAME}/`
+      );
+    }
+
     return client.s3_fetch(
-      `https://${env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com/`
+      `${s3Endpoint}/`
     );
   } catch (e) {
     return new Response(e.toString(), { status: 500 });
